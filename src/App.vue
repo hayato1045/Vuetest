6bd8ed62ce33d3511dde0a182fccbe34cea587a7
hayato1045 <hayatobasuke88gmail.com>
2020-05-06 21:23:15 +0900

    up

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+  <div id="app" class="container">
+    <img class="logo" alt="Vue logo" src="./assets/logo.png" />
     <todo-list></todo-list>
   </div>
 </template>
@@ -18,5 +18,19 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+#app {
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 24px;
+}
+.logo {
+  display: block;
+  margin: 20px auto;
+  height: 75px;
+}
 </style>
